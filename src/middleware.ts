8a13dd9146a06aa477aa20025bcc60481f5680f5
Prayas Mazumder <128576227+captainprice27@users.noreply.github.com>
2024-09-10 23:17:38 +0530

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
+
+export default authkitMiddleware();
+
+// Match against pages that require authentication
+// Leave this out if you want authentication on every page in your application
+export const config = { matcher: 
+    ['/',
+    '/new-listing',
+    '/new-company',
+    '/new-listing/:orgId*',
+    '/jobs/:orgId*',
+    '/jobs/edit/:jobId*',
+    '/show/:jobId*',
+] };
